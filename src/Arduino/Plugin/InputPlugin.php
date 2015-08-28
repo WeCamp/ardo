9badadb2a8e7f9de33c06cda874ed476d89802ea
Ben Peachey <potherca@gmail.com>
2015-08-28 11:21:27 +0200

    Adds logger to the Arduino plugin.

--- src/Arduino/Plugin/InputPlugin.php
+++ src/Arduino/Plugin/InputPlugin.php
@@ -3,14 +3,18 @@
 namespace WeCamp\Ardo\Arduino;
 
 use GuzzleHttp\Client;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
 use WeCamp\Ardo\Plugin\MessageInterface;
 use WeCamp\Ardo\Plugin\OutputInterface;
 use WeCamp\Ardo\Slack\Messages\InputMessage;
 
-class InputPlugin implements OutputInterface
+class InputPlugin implements OutputInterface, LoggerAwareInterface
 {
     /** @var Client */
     private $client;
+    /** @var LoggerInterface */
+    private $logger;
 
     public function __construct(Client $client)
     {
@@ -20,15 +24,27 @@ class InputPlugin implements OutputInterface
     final public function handleMessage(MessageInterface $message)
     {
         if ($this->shouldCallArduino($message)) {
+            $this->logger->info($message, ['class' => self::class]);
             $this->callArduino($this->client, $message->toString());
         }
+    }
+
+    /**
+     * Sets a logger instance on the object
+     *
+     * @param LoggerInterface $logger
+     * @return null
+     */
+    final public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
     }
 
     private function callArduino(Client $client, $message)
     {
         list($url, $parameter) = explode(' ', $message, 2);
         $response = $client->request('get', $url, ['params' => $parameter]);
-
+        $this->logger->info($url, ['class' => self::class, 'response' => $response]);
         return $response;
     }
 
