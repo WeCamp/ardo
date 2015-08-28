84454adffd5d6a9c2407241541e550265c5e5f92
Ben Peachey <potherca@gmail.com>
2015-08-28 10:44:05 +0200

    Adds Plugin for the Arduino.

--- src/Arduino/Plugin/InputPlugin.php
+++ src/Arduino/Plugin/InputPlugin.php
@@ -2,14 +2,44 @@
 
 namespace WeCamp\Ardo\Arduino;
 
+use GuzzleHttp\Client;
 use WeCamp\Ardo\Plugin\MessageInterface;
 use WeCamp\Ardo\Plugin\OutputInterface;
+use WeCamp\Ardo\Slack\Messages\InputMessage;
 
 class InputPlugin implements OutputInterface
 {
+    /** @var Client */
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
     final public function handleMessage(MessageInterface $message)
     {
+        if ($this->shouldCallArduino($message)) {
+            $this->callArduino($this->client, $message->toString());
+        }
+    }
 
+    private function callArduino(Client $client, $message)
+    {
+        list($url, $parameter) = explode(' ', $message, 2);
+        $response = $client->request('get', $url, ['params' => $parameter]);
+
+        return $response;
+    }
+
+    /**
+     * @param MessageInterface $message
+     * @return bool
+     */
+    private function shouldCallArduino(MessageInterface $message)
+    {
+        $isEmpty = $message->isEmpty();
+        return $message instanceof InputMessage && $isEmpty === false;
     }
 }
 /*EOF*/
--- src/Arduino/Tests/InputPluginTest.php
+++ src/Arduino/Tests/InputPluginTest.php
@@ -2,24 +2,51 @@
 
 namespace WeCamp\Ardo\Arduino\Tests;
 
+use GuzzleHttp\Client;
 use WeCamp\Ardo\Arduino\InputPlugin;
+use WeCamp\Ardo\Plugin\MessageInterface;
+use PHPUnit_Framework_MockObject_MockObject as Mock;
+use WeCamp\Ardo\Slack\Messages\InputMessage;
 
 /**
  * Tests for the InputPlugin class
  *
- * @coversDefaultClass WeCamp\Arduino\InputPlugin
+ * @coversDefaultClass WeCamp\Ardo\Arduino\InputPlugin
+ * @covers ::__construct
+ * @covers ::<!public>
  */
 class InputPluginTest extends \PHPUnit_Framework_TestCase
 {
     /** @var InputPlugin */
     private $plugin;
+    /** @var Client|Mock */
+    private $mockClient;
 
+    /**
+     * Get things in order...
+     */
     final public function setup()
     {
-        $this->plugin = new InputPlugin();
+        /** @var Client|Mock $mockClient */
+        $this->mockClient = $this->getMock(Client::class);
+        $this->plugin = new InputPlugin($this->mockClient);
     }
 
-    final function testShouldComplainWhenNotGivenMessage()
+    final public function testPluginShouldReceiveHttpClientWhenInstantiated()
+    {
+        $this->setExpectedExceptionRegExp(
+            \PHPUnit_Framework_Error::class,
+            '/none given/'
+        );
+
+        /** @noinspection PhpParamsInspection */
+        new InputPlugin();
+    }
+
+    /**
+     * @covers ::handleMessage
+     */
+    final function testPluginShouldComplainWhenNotGivenMessage()
     {
         $plugin = $this->plugin;
 
@@ -30,6 +57,79 @@ class InputPluginTest extends \PHPUnit_Framework_TestCase
 
         /** @noinspection PhpParamsInspection */
         $plugin->handleMessage();
+    }
+
+    /**
+     * @covers ::handleMessage
+     */
+    final function testPluginShouldOnlyCallArduinoWhenGivenSlackMessage()
+    {
+        $plugin = $this->plugin;
+
+        /** @var MessageInterface|Mock $mockMessage */
+        $mockMessage = $this->getMock(MessageInterface::class);
+        $mockMessage->expects($this->never())
+            ->method('toString')
+        ;
+
+        $plugin->handleMessage($mockMessage);
+    }
+
+    /**
+     * @covers ::handleMessage
+     */
+    final function testPluginShouldOnlyCallArduinoWhenGivenSlackMessageIsNotEmpty()
+    {
+        $plugin = $this->plugin;
+
+        /** @var MessageInterface|Mock $mockMessage */
+        $mockMessage = $this->getMockBuilder(InputMessage::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+        $mockMessage->expects($this->exactly(1))
+            ->method('isEmpty')
+            ->willReturn(true);
+        ;
+
+        $plugin->handleMessage($mockMessage);
+    }
+
+    /**
+     * @covers ::handleMessage
+     */
+    final function testPluginShouldPassMessageToArduinoWhenGivenSlackMessage()
+    {
+        $plugin = $this->plugin;
+
+        $mockCommand = 'mockCommand';
+        $mockParameters = 'AAaAAaAAaa VBBbBBbB';
+        $expected = sprintf('%s %s', $mockCommand, $mockParameters);
+
+        $mockClient = $this->mockClient;
+
+        /** @var InputMessage|Mock $mockMessage */
+        $mockMessage = $this->getMockBuilder(InputMessage::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $mockMessage->expects($this->exactly(1))
+            ->method('toString')
+            ->willReturn($expected)
+        ;
+
+        $mockMessage->expects($this->exactly(1))
+            ->method('isEmpty')
+            ->willReturn(false);
+        ;
+
+        $mockClient->expects($this->exactly(1))
+            ->method('request')
+            ->with('get', $mockCommand, ['params' => $mockParameters])
+        ;
+
+        $plugin->handleMessage($mockMessage);
     }
 }
 
